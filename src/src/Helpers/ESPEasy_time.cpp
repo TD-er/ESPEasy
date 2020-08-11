fed4078f7b5711979b85869d6dfbfc48e119156e
Gijs Noorlander <gijs.noorlander@gmail.com>
2020-08-12 01:24:24 +0200

    [ESPEasy-now] Fix merge issues.

--- src/src/Helpers/ESPEasy_time.cpp
+++ src/src/Helpers/ESPEasy_time.cpp
@@ -17,33 +17,6 @@
 #include <time.h>
 
 
-String toString(timeSource_t timeSource)
-{
-  switch (timeSource) {
-    case timeSource_t::GPS_PPS_time_source:     return F("GPS PPS");
-    case timeSource_t::GPS_time_source:         return F("GPS");
-    case timeSource_t::NTP_time_source:         return F("NTP");
-    case timeSource_t::Manual_set:              return F("Manual");
-    case timeSource_t::ESP_now_peer:            return F("ESPEasy-NOW peer");
-    case timeSource_t::Restore_RTC_time_source: return F("RTC at boot");
-    case timeSource_t::No_time_source:          return F("No time set");
-  }
-  return F("Unknown");
-}
-
-bool isExternalTimeSource(timeSource_t timeSource)
-{
-  switch (timeSource) {
-    case timeSource_t::GPS_PPS_time_source:
-    case timeSource_t::GPS_time_source:    
-    case timeSource_t::NTP_time_source:
-    case timeSource_t::Manual_set:
-      return true;
-    default:
-      return false;
-  }
-}
-
 ESPEasy_time::ESPEasy_time() {
   memset(&tm,      0, sizeof(tm));
   memset(&tsRise,  0, sizeof(tm));
